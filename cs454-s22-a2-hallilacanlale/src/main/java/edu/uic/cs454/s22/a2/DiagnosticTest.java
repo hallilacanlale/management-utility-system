f51fd21fb258054203d40272cd51d8558469e04c
Halli Lacanlale <57775171+hallilacanlale@users.noreply.github.com>
2023-03-29 13:09:16 -0500

    Delete cs454-s22-a2-hallilacanlale directory

--- cs454-s22-a2-hallilacanlale/src/main/java/edu/uic/cs454/s22/a2/DiagnosticTest.java
+++
@@ -1,8 +0,0 @@
-package edu.uic.cs454.s22.a2;
-
-public interface DiagnosticTest {
-    enum Status { READY , SAMPLED , POSITIVE , NEGATIVE , INVALID }
-
-    Status getStatus();
-    void setStatus(Status status);
-}
